c2089d5a35cfed0e9f56cc27b35ba9d0baa3fb5b
Rifqifauu <120528673+Rifqifauu@users.noreply.github.com>
2025-08-10 08:33:04 +0700

    Update auth card dan select

--- resources/views/components/form/select-group.blade.php
+++ resources/views/components/form/select-group.blade.php
@@ -107,7 +107,7 @@
         x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100"
         x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100"
         x-transition:leave-end="transform opacity-0 scale-95"
-        class="absolute z-10 w-full mt-1 bg-white rounded-md shadow-lg" style="display: none;">
+        class="absolute z-100 w-full mt-1 bg-white rounded-md shadow-lg" style="display: none;">
         <div class="p-2">
             <input type="search" x-model.debounce.300ms="search" placeholder="Cari..."
                 class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-indigo-500"
